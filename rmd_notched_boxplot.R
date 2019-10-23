a4e34e6bd86ec109c429b35ece322f8f0d049762
Tamas Schauer <Tamas@Tamass-MacBook-Pro.local>
2019-10-23 14:30:14 +0200

    restructure main page

---
+++ rmd_notched_boxplot.R
@@ -0,0 +1,25 @@
+---
+title: "Data Binning and Correlation"
+output: 
+    html_document:
+    toc: TRUE
+---
+    
+    <style>
+    pre {
+        overflow-x: auto;
+    }
+pre code {
+    word-wrap: normal;
+    white-space: pre;
+}
+a {
+    font-size: 16px;
+}
+</style>
+    
+    ```{r global options, include = FALSE}
+knitr::opts_chunk$set(echo=TRUE, include = TRUE, warning=FALSE, message=FALSE)
+```
+
+---
